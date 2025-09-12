4dc982cf3dc5ce9747c9ee7ca9a1dbbf38e33222
Nacho <ifuenzalida@usm.cl>
2025-09-12 14:21:16 -0300

    creando tablas

---
+++ api_expres/src/models/Producto.ts
@@ -0,0 +1,22 @@
+import { Column, DataType, Model, Table } from "sequelize-typescript";
+
+@Table({tableName: "productos" })
+class Producto extends Model{
+
+    @Column({type: DataType.SMALLINT(), primaryKey: true, allowNull: false, field:"cod_producto" })
+        declare codProducto: string
+
+    @Column({type: DataType.STRING(30), allowNull: false, field:"nombre_producto" })
+        declare nombreProducto: string
+
+    @Column({type: DataType.INTEGER(), allowNull: false, field:"precio_unitario" })
+        declare precioUnitario: string
+
+    @Column({type: DataType.STRING(50), allowNull: true, field:"descripcion" })
+        declare descripcion: string
+
+    @Column({type: DataType.SMALLINT(), allowNull: true, field:"stock" })
+        declare stock: string
+}
+
+
