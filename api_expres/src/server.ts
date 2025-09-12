6a78c4d1b2de9f8a0ad4207922f6e138333c4525
Nacho <ifuenzalida@usm.cl>
2025-09-11 23:59:54 -0300

    modifique

--- api_expres/src/server.ts
+++ api_expres/src/server.ts
@@ -3,6 +3,6 @@ import  Express  from "express"
 const server = Express()
 
 server.get('/', (request, response)=>{
-    response.send('hola mundo express')
+    response.send('hola mundo API express')
 })
 export default server
