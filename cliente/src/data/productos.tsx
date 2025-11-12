9c3a63722e5a6b259ca0c778a75a6bd4e3d1e59f
Nacho <ifuenzalida@usm.cl>
2025-11-12 18:37:14 -0300

    comeback

--- cliente/src/data/productos.tsx
+++ cliente/src/data/productos.tsx
@@ -2,8 +2,9 @@ export const productos = [
   { titulo: "Liquido de frenos", descripcion: "",precio: "3000", imagen: "/public/images/liquido_freno.png" },
   { titulo: "Aceite 10W40", descripcion: "",precio: "2000", imagen: "/public/images/aceite_motor.png" },
   { titulo: "Aceite 5W30", descripcion: "",precio: "10000", imagen: "/public/images/5w30.png" },
-  { titulo: "Filtro de aire", descripcion: "",precio: "", imagen: "/public/images/filtroaire.png" },
-  { titulo: "Filtro de aceite", descripcion: "",precio: "", imagen: "/public/images/filtroaceite.png" },
-  { titulo: "Refrigerante", descripcion: "",precio: "", imagen: "/public/images/refrigerante.png" },
+  { titulo: "Filtro de aire", descripcion: "",precio: "7900", imagen: "/public/images/filtroaire.png" },
+  { titulo: "Filtro de aceite", descripcion: "",precio: "2200", imagen: "/public/images/filtroaceite.png" },
+  { titulo: "Refrigerante", descripcion: "",precio: "3990", imagen: "/public/images/refrigerante.png" },
   { titulo: "Camaras", descripcion: "", precio: "3000", imagen: "/public/images/camara.png" },
+  
 ];
