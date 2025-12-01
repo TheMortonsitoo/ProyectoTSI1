7b11a40c06a4e478e3a567b9c9e01608910d5d9e
Tommy <tsaavedrai@usm.cl>
2025-12-01 18:24:45 -0300

    ahora shi

--- cliente/src/pages/Admin/PaginaAdmin.tsx
+++ cliente/src/pages/Admin/PaginaAdmin.tsx
@@ -1,43 +1,13 @@
-import { useState } from "react";
-import axios from "axios";
 import { useNavigate } from "react-router-dom";
-
-
 
 const AdminPanel = () => {
 
   const navigate = useNavigate();
-  const [mensaje, setMensaje] = useState("");
-
-  const sincronizarAdmins = async () => {
-    try {
-      const token = localStorage.getItem("token");
-      console.log("Token usado:", token);
-
-      const response = await axios.post(
-        "http://localhost:3000/api/sincronizar-admins",
-        {},
-        {
-          headers: {
-            Authorization: `Bearer ${token}`,
-          },
-        }
-      );
-
-      setMensaje(response.data.mensaje);
-    } catch (error: any) {
-      console.error("Error al sincronizar:", error);
-      setMensaje(error.response?.data?.mensaje || error.message);
-    }
-  };
 
   return (
     <div>
       <h1>Panel de Administración</h1>
       <p>Solo accesible para usuarios con rol admin.</p>
-
-
-      {mensaje && <p>{mensaje}</p>}
 
       <button onClick={() => navigate('/agregar-producto')}>Agregar producto</button>
 
